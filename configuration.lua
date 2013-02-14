0a4362a3b3f063ac2033a94726741c926ced69b8
whoja <social@whoja.com>
2013-02-14 04:02:55 -0500

    [yeah]

---
+++ configuration.lua
@@ -0,0 +1,239 @@
+configuration =  
+{
+	 
+	{
+		setting = "window",
+		value = 0,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "shadows",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "postprocessing",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "squadcontrol",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "mousescroll",
+		value = 128,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "ribbons",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "musicvolume",
+		value = 255,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "unitresponses",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "playercolour",
+		value = 0,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "modeldetail",
+		value = 256,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "effectsfidelity",
+		value = 3,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "texturedetail",
+		value = 0,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "showpaths",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "treedetail",
+		value = 1,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "physics",
+		value = 3,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "reflections",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "showgametime",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "sfxvolume",
+		value = 255,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "objectscarring",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "width",
+		value = 1360,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "soundfrequency",
+		value = 44100,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "stickyselection",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "shrubsdetail",
+		value = 1,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "shaderquality",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "showsubtitles",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "mastervolume",
+		value = 255,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "screengamma",
+		value = 3,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "soundconfig",
+		value = 7,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "raindetail",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "antialiasing",
+		value = 0,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "envreverbchannels",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "height",
+		value = 768,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "squadeventcues",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "modelquality",
+		value = 1,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "effectsdensity",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "helptext",
+		value = 1,
+		valueType = 1,
+	},
+	 
+	{
+		setting = "terraindetail",
+		value = 2,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "speechvolume",
+		value = 255,
+		valueType = 2,
+	},
+	 
+	{
+		setting = "soundvoices",
+		value = 128,
+		valueType = 2,
+	},
+}
+version = 1007
+
---
+++ playercfg.lua
@@ -0,0 +1,53 @@
+HelpText =  
+{
+	GlobalEnable = true,
+	OnlineGameSetup = true,
+	RelicOnlineNewsScreen = true,
+}
+Performance =  
+{
+	Avg_2_0 = 0,
+	Avg_2_1 = 0,
+	Avg_2_2 = 0,
+	Avg_2_3 = 0,
+	Avg_2_4 = 0,
+	Max_2_0 = 0,
+	Max_2_1 = 0,
+	Max_2_2 = 0,
+	Max_2_3 = 0,
+	Max_2_4 = 0,
+}
+Rewards =  
+{
+}
+SCAR =  
+{
+}
+SinglePlayer =  
+{
+	LastPlayedCampaigns =  
+	{
+		WW2 = "coh",
+	},
+	sp_campaign = "coh",
+	sp_difficulty = 1,
+	sp_selectedmap = "m01_d-day",
+}
+mp_game_played_info =  
+{
+	last_played_skirmish = "",
+}
+relic_online_info =  
+{
+}
+vehicle_activation =  
+{
+	DLC1_5349219 = false,
+	DLC2_5349219 = false,
+	DLC3_5349219 = false,
+}
+vista_fix_reminder =  
+{
+	hide_hotfix_reminder = false,
+}
+
